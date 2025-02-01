89da2e6377316d30faa3d2924375b58d97f63d36
ImJaePang <narushel@naver.com>
2025-02-02 08:17:07 +0900

    no style

---
+++ src/components/DetailProjecPage.jsx
@@ -0,0 +1,30 @@
+
+// 제목
+// 삭제버튼
+// 듀데이트
+// 디스크립션
+// 태스크
+
+// 인풋박스, 애드 테스크
+
+// 프랙티스 클리어
+// 프랙티스 클리어
+// 프랙티스 클리어
+
+export default function(){
+
+    return <>
+        <h1>제목</h1>
+        <button>Delete</button>
+        <h3 className="text-gray-500">Dec 29, 2024</h3>
+        <p>descrption any...</p>
+        <hr className="border-gray-500"/>
+        <h1>Tasks</h1>
+
+        <input className="bg-gray-500" />
+        <button>Add Task</button>
+        
+
+    
+    </>
+}
